4c29c9573dafaf8152d9765e5b8823d9340e6067
Caitlyn & Sean Sassenrath <pair+heycait.seansassenrath@devbootcamp.com>
2015-05-08 19:30:57 -0700

    have a working login and signup page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,3 +7,38 @@ get '/' do
   erb :index
 end
 
+get '/users/new' do
+  erb :user_new
+  # use single quotes inside the double quotes to override white space delimeter
+end
+
+post '/users' do
+  user = User.new(email: params[:email])
+  user.password = params[:password]
+
+  if user.save
+    redirect '/homepage'
+  else
+    status 400
+    "you fucked up"
+  end
+end
+
+get '/login/new' do
+  erb :login
+end
+
+post '/login' do
+  user = User.where(email: params[:email]).first
+  if user && user.password == params[:password]
+    login(user)
+    redirect '/homepage'
+  else
+    status 400
+    "You aint down"
+  end
+end
+
+get '/homepage' do
+  erb :homepage
+end
